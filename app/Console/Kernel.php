9ddd95208fffdf91b8a3b9526a3bdfe963673d12
Gabrielkosinski <gabriel.kosinski.cwb@gmail.com>
2025-07-28 22:16:54 -0300

    fix: adiciona Kernel padrão e garante migração de carros correta

---
+++ app/Console/Kernel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    // Você pode registrar comandos personalizados aqui, se necessário
+}
